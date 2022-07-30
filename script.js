9c01a2a4199fadf073320912357be01418730606
Ayush-Tiw <99787142+Ayush-Tiw@users.noreply.github.com>
2022-07-30 15:57:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,39 @@
+document.body.innerHTML=`<h1  class="countdown"></h1>`;
+
+const countdown=document.querySelector(".countdown");
+
+let time=10;
+
+setTimeout(()=>{
+    countdown.innerText=time--;
+    setTimeout(()=>{
+        countdown.innerText=time--;
+        setTimeout(()=>{
+            countdown.innerText=time--;
+            setTimeout(()=>{
+                countdown.innerText=time--;
+                setTimeout(()=>{
+                    countdown.innerText=time--;
+                    setTimeout(()=>{
+                        countdown.innerText=time--;
+                        setTimeout(()=>{
+                            countdown.innerText=time--;
+                            setTimeout(()=>{
+                                countdown.innerText=time--;
+                                setTimeout(()=>{
+                                    countdown.innerText=time--;
+                                    setTimeout(()=>{
+                                        countdown.innerText=time--;
+                    setTimeout(()=>{
+    countdown.innerText="Happy Independence Day";
+                    },1000)
+                },1000)
+            },1000)
+        },1000)
+    },1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
+},1000)
